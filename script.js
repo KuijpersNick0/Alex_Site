f1897c82e0a06305f0ac1e7de81132e369ad0a49
Nick Kuijpers <73790831+KuijpersNick0@users.noreply.github.com>
2022-10-07 16:03:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '<YOUR_CLIENT_ID>';
+const API_KEY = '<YOUR_API_KEY>';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
 //Contact Form en PHP
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
